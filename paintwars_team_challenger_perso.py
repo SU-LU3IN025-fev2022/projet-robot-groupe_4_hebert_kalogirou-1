998c829731e8b6dbdc190e30e36eb5b5c5c12475
thomashebert99 <thomas.hebert99@gmail.com>
2022-04-21 22:04:34 +0200

    update

--- paintwars_team_challenger_perso.py
+++ paintwars_team_challenger_perso.py
@@ -37,7 +37,7 @@ def step(robotId, sensors):
     
     def hateWall():
         translation = 1
-        rotation = (-1) * sensors["sensor_front_left"]["distance"] + (1) * sensors["sensor_front_right"]["distance"] + (-1) * sensors["sensor_left"]["distance"] + sensors["sensor_right"]["distance"]
+        rotation = (-1) * sensors["sensor_front_left"]["distance_to_wall"] + (1) * sensors["sensor_front_right"]["distance_to_wall"] + (-1) * sensors["sensor_left"]["distance_to_wall"] + sensors["sensor_right"]["distance_to_wall"]
         return translation, rotation
     
     def loveBot():
@@ -86,16 +86,7 @@ def step(robotId, sensors):
     
     elif sensors["sensor_front"]["distance_to_wall"] < 1:
         rotation = random.choice([-1, 1])
-        return translation, rotation
-    
-    elif sensors["sensor_front"]["distance"] < 0.2 or sensors["sensor_front_left"]["distance"] < 0.2 or sensors["sensor_front_right"]["distance"] < 0.2 :
-        if sensors["sensor_front_left"]["distance"] < sensors["sensor_front_right"]["distance"] : # Bloqué à  gauche
-            rotation = 1  
-
-        else: # Bloqué à droite
-            rotation = -1  
-
-        return translation , rotation 
+        return translation, rotation 
     
     else:
         return hateWall()
